5d85d0beaffc61841ef6528642ec18b8ec3248d0
Samson Yuwono <samson.yuwono@gmail.com>
2017-06-22 14:54:37 +0000

    removed movie_year

--- lib/top_movies/movie.rb
+++ lib/top_movies/movie.rb
@@ -35,7 +35,7 @@ class TopMovies::Movie
           movie.title= row.css(".articleLink").text.split("(")[0].strip
           movie.ranking= row.css(".bold").text.gsub(".","")
           movie.year= row.css(".articleLink").text.strip.scan(/\((\w+)/).flatten.join
-          movie.rating= row.css(".tMeterScore").text.gsub("\u00A0", " ")
+          movie.rating= row.css(".tMeterScore").text.gsub("\u00A0", " ").strip
           movie.reviews= row.css(".hidden-xs").text
         end
       end
